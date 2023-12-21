e6ec523afeaa9b31a5b857e22664945080446174
Diivvuu <divyanshubawa9@gmail.com>
2023-12-21 13:58:55 +0530

    first and last occurence by binary search

--- lecture 13 (binary search interview questions)/classroom/firstAndLastPositionOfanElement.cpp
+++ lecture 13 (binary search interview questions)/classroom/firstAndLastPositionOfanElement.cpp
@@ -1,37 +1,37 @@
-#include <bits/stdc++.h> 
-using namespace std;
-int firstOccurence(vector<int>& arr, int n, int key) {
-    int start = 0, end = n-1;
-    int mid = start + (end - start)/2;
+int firstOccurence(vector<int>& arr, int size, int element){
     int ans = -1;
-    while(start <= end) {
-        if (arr[mid] == key) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start)/2;
+    while(start <= end){
+        if(arr[mid] == element){
             ans = mid;
             end = mid - 1;
         }
-        else if(arr[mid] < key) {
-            start = mid + 1;
-        }
-        else if(arr[mid] > key) {
+        else if(arr[mid] > element){
             end = mid - 1;
+        }
+        else {
+            start = mid + 1;
         }
         mid = start + (end - start)/2;
     }
     return ans;
 }
-int LastOccurence(vector<int>& arr, int n, int key) {
-    int start = 0, end = n-1;
-    int mid = start + (end - start)/2;
+int lastOccurence(vector<int>& arr, int size, int element){
     int ans = -1;
-    while(start <= end) {
-        if(arr[mid] == key) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start)/2;
+    while(start <= end){
+        if(arr[mid] == element){
             ans = mid;
             start = mid + 1;
         }
-        else if (arr[mid] < key) {
+        else if(arr[mid] < element){
             start = mid + 1;
-        }
-        else{
+        } 
+        else {
             end = mid - 1;
         }
         mid = start + (end - start)/2;
@@ -41,19 +41,8 @@ int LastOccurence(vector<int>& arr, int n, int key) {
 pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
 {
     // Write your code here
-    pair <int, int> p;
+    pair<int, int> p;
     p.first = firstOccurence(arr, n, k);
-    p.second = LastOccurence(arr, n, k);
+    p.second = lastOccurence(arr, n, k);
     return p;
 }
-// int main() {
-//     int even[6] = {1, 3, 3, 3, 4, 6};
-//     cout << "First occurence of 3 is " << firstOccurence(even, 6, 3) << endl;
-//     cout << "Last occurence of 3 is " << LastOccurence(even, 6, 3);
-
-// }
-
-// pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
-// {
-    // Write your code here
-// }
